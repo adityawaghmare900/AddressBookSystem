784bf1f8fbcb5ffa71e42feac98861aeeb32ff3f
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-24 15:41:19 +0530

    [aditya]: Create Contact class with getters and setter

---
+++ Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateContact_UC1
+{
+    internal class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address {  get; set; }
+        public string City { get; set; }
+        public string state {  get; set; }
+        public int zipcode { get; set; }
+        public long Phone {  get; set; }
+        public string emailID { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CreateContact_UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
